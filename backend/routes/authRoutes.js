d72d4d0ade3663d6508809d2057927e59c922b71
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-04 12:47:49 +0200

    first commit

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express'); 
+const router = express.Router();
+const authControllers = require('../controllers/authControllers'); 
+
+// /auth/login
+router.post('/login', isNotLoggedIn, authControllers.findUser);
+
+// /auth/register
+router.post('/register', isNotLoggedIn, authControllers.createUser);
+
+// /auth/logout
+router.post('/logout', isLoggedIn,  authControllers.logout);
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {  
+      return next()
+    }
+    res.redirect('/')
+  }
+  
+  function isNotLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+
+module.exports = router;
