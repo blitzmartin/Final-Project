e18fe6033e3adb7cf23ba318792b6b3aafb8e9dc
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-07 18:03:43 +0200

    small changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,8 @@ import { FaLinkedinIn } from "react-icons/fa";
 export default function Footer(){
     return(
         <div className="Footer">
-            <a href="https://github.com/blitzmartin"><FaGithub size={20} /></a>
-            <a href="https://www.linkedin.com/in/demartinodev/"><FaLinkedinIn size={20} /></a>
+            <a href="https://github.com/blitzmartin" target="_blank" ><FaGithub size={20} /></a>
+            <a href="https://www.linkedin.com/in/demartinodev/" target="_blank" ><FaLinkedinIn size={20} /></a>
             <h4>Annalisa De Martino &copy; 2022</h4>
         </div>
     )
