071c3e3d4e797fd9b15667c7afca533f718b864e
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-07 11:43:36 +0200

    fixed home showing twice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header'
-import Main from './components/Main'
 import Footer from './components/Footer'
 import NotFound from './pages/NotFound'
+import Home from './pages/Home'
 import NewPost from './pages/NewPost'
 
 function App() {
   return (
     <div className="App">
+     
       <Router>
         <Header />
-        <Main />
-        <Footer />
-        
-
         <Routes>
           <Route path='*' element={<NotFound />} />
-          <Route path='/' element={<Main />} />
+          <Route path='/' element={<Home/>} />
           <Route path='/newpost' element={<NewPost />} />
         </Routes>
       </Router>
+      <Footer />
     </div>
   );
 }
