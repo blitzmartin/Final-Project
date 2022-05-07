001e092cb145f160ecd04282209f5d2b7cafd490
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-07 17:33:54 +0200

    install react icons, added icons to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,11 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 export default function Footer(){
     return(
         <div className="Footer">
+            <a href="https://github.com/blitzmartin"><FaGithub size={20} /></a>
+            <a href="https://www.linkedin.com/in/demartinodev/"><FaLinkedinIn size={20} /></a>
             <h4>Made by Annalisa De Martino &copy; 2022</h4>
         </div>
     )
