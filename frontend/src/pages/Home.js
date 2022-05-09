e6f43bbf05b515de0d184b4d251c248636a444cb
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-09 12:06:39 +0200

    posts showing in homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Main() {
 
@@ -13,6 +13,10 @@ export default function Main() {
             .catch(err => console.log(err))
         return
     }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
 
     return (
         <div className="mainContent">
